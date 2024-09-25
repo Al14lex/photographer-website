04381f5a3d098614b49bf9c35c0f2608c731a94c
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2024-09-25 18:02:44 +0200

    added styles for header

--- src/js/home.js
+++ src/js/home.js
@@ -1,20 +1,33 @@
+//============ підсвічування назви активної сторінки====
 
-    const modal = document.getElementById("modal");
-    const span = document.getElementById("close");
+const currentPage = window.location.pathname.split("/").pop(); 
+const menuLinks = document.querySelectorAll('.nav-list a');
 
-    document.querySelector(".contact-form").onsubmit = function(event) {
-        event.preventDefault(); 
-        modal.style.display = "block";
-        this.reset();
-    };
+menuLinks.forEach(link => {
+  const linkPage = link.getAttribute('href').split("/").pop();
 
-    span.onclick = function() {
+  if (linkPage === currentPage) {
+    link.classList.add('active');
+  }
+});
+
+//=========== модальне вікто при відправці форми========  
+const modal = document.getElementById("modal");
+const span = document.getElementById ("close");
+
+document.querySelector(".contact-form").onsubmit = function(event) {
+    event.preventDefault(); 
+    modal.style.display = "block";
+    this.reset();
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
-    };
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
+    }
+};
 
