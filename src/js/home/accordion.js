768f5a406c2367e3720a241cd91dab1685364413
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-01-22 17:04:07 +0100

    added social links

--- src/js/home/accordion.js
+++ src/js/home/accordion.js
@@ -1,20 +1,18 @@
 document.querySelectorAll('.accordion-header').forEach(header => {
     header.addEventListener('click', () => {
-        const content = header.nextElementSibling; // Знайти відповідний список
+        const content = header.nextElementSibling;
         const isOpen = header.classList.contains('open');
 
-        // Закрити всі інші акордеони
         document.querySelectorAll('.accordion-header.open').forEach(openHeader => {
             openHeader.classList.remove('open');
             openHeader.nextElementSibling.classList.remove('open');
             openHeader.nextElementSibling.style.maxHeight = '0';
         });
 
-        // Відкрити або закрити поточний акордеон
         if (!isOpen) {
             header.classList.add('open');
             content.classList.add('open');
-            content.style.maxHeight = content.scrollHeight + 'px'; // Розкрити до висоти контенту
+            content.style.maxHeight = content.scrollHeight + 'px'; 
         } else {
             header.classList.remove('open');
             content.classList.remove('open');
