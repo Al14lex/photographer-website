a43e8b570a25b9e04f74cbfaa9a78f87e154f579
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-01-22 17:04:07 +0100

    added social links

--- src/js/home/mobile-menu.js
+++ src/js/home/mobile-menu.js
@@ -1,11 +1,35 @@
+
 const burgerMenu = document.querySelector('.burger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.close-menu');
+const menuLinks = document.querySelectorAll('.mobile-menu-list a'); // Всі посилання в меню
+
 
 burgerMenu.addEventListener('click', () => {
     mobileMenu.classList.add('open');
 });
 
+
 closeMenu.addEventListener('click', () => {
     mobileMenu.classList.remove('open');
 });
+
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href'); 
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+
+            mobileMenu.classList.remove('open');
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start', 
+            });
+        }
+    });
+});
+
