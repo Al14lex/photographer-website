3db24b6b376a55d4f2d69a329cf6ffe432e2d059
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-01-26 17:02:51 +0100

    improving styles

--- src/js/home/about-me.js
+++ src/js/home/about-me.js
@@ -1,33 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
     const aboutSection = document.querySelector('.about-container');
     const aboutImg = document.querySelector('.about-img');
-    const aboutTextLines = document.querySelectorAll('.about-content p');
+    const aboutImgBec = document.querySelector('.about-bec-img');
+    const aboutHeader = document.querySelector('.about-content h2');
+    const aboutTextLines = document.querySelectorAll('.about-text .line');
+    const aboutLink = document.querySelector('.about-link');
+    const aboutTitle = document.querySelector('.about-title');
+    const aboutTitle2 = document.querySelector('.about-title2'); // Додали
 
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     aboutSection.classList.add('visible');
-                    aboutImg.classList.add('visible');
+                    
+                    setTimeout(() => {
+                        aboutImg.classList.add('visible');
+                        aboutImgBec.classList.add('visible');
+                        aboutTitle.classList.add('visible');
+                    }, 300);
+
+                    setTimeout(() => {
+                        aboutTitle2.classList.add('visible'); // Показуємо заголовок 2
+                    }, 400);
+
                     aboutTextLines.forEach((line, index) => {
                         setTimeout(() => {
                             line.classList.add('line-visible');
-                        }, index * 350); 
+                        }, 600 + index * 200);
                     });
+
+                    setTimeout(() => {
+                        aboutLink.classList.add('visible');
+                    }, 1200);
                 } else {
                     aboutSection.classList.remove('visible');
                     aboutImg.classList.remove('visible');
+                    aboutImgBec.classList.remove('visible');
+                    aboutTitle.classList.remove('visible');
+                    aboutTitle2.classList.remove('visible'); // Ховаємо заголовок 2
                     aboutTextLines.forEach((line) => {
                         line.classList.remove('line-visible');
                     });
+                    aboutLink.classList.remove('visible');
                 }
             });
         },
         {
-            threshold: 0.5,
+            threshold: 0.4,
         }
     );
 
     observer.observe(aboutSection);
 });
-
