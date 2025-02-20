3d0e11d5fe1f2d377891cc04485fceaee17c8b6a
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-02-20 13:05:27 +0100

    settings for vite2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import { globSync } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
@@ -14,7 +14,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/**/*.html'),
+        input: globSync('./src/**/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -28,7 +28,7 @@ export default defineConfig(({ command }) => {
     },
     server: {
       fs: {
-        allow: ['..'], // Дозволяємо доступ до кореневого каталогу
+        allow: ['..'],
       },
     },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
