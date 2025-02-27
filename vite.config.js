9da457f1f80ffc95e5865996691d0cba29d0bd75
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-02-27 13:36:49 +0100

    Fix Vite outDir issue

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,39 @@
+// import { defineConfig } from 'vite';
+// import { glob } from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+
+// export default defineConfig(async ({ command }) => {
+//   return {
+//     define: {
+//       global: {},
+//       _global: {},
+//     },
+//     root: 'src',
+//     publicDir: '../public', 
+//     build: {
+//       sourcemap: true,
+//       rollupOptions: {
+//         input: await glob('./src/**/*.html'),
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: 'commonHelpers.js',
+//         },
+//       },
+//       outDir: '../dist',
+//     },
+//     server: {
+//       fs: {
+//         allow: ['..'],
+//       },
+//     },
+//     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+//   };
+// });
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
@@ -13,6 +49,8 @@ export default defineConfig(async ({ command }) => {
     publicDir: '../public', 
     build: {
       sourcemap: true,
+      outDir: '../dist', // 🔹 Можливо, треба змінити на просто 'dist'
+      emptyOutDir: true, // ✅ Додаємо, щоб очищати `dist` перед збіркою
       rollupOptions: {
         input: await glob('./src/**/*.html'),
         output: {
@@ -24,7 +62,6 @@ export default defineConfig(async ({ command }) => {
           entryFileNames: 'commonHelpers.js',
         },
       },
-      outDir: '../dist',
     },
     server: {
       fs: {
