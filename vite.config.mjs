93da7a2066d6daf364491232d990f4a76263bb2f
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-02-27 13:52:33 +0100

    Fix Vite CJS deprecation warning

---
+++ vite.config.mjs
@@ -0,0 +1,38 @@
+
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+
+export default defineConfig(async ({ command }) => {
+  return {
+    define: {
+      global: {},
+      _global: {},
+    },
+    root: 'src',
+    publicDir: '../public', 
+    build: {
+      sourcemap: true,
+      outDir: '../dist', // 🔹 Можливо, треба змінити на просто 'dist'
+      emptyOutDir: true, // ✅ Додаємо, щоб очищати `dist` перед збіркою
+      rollupOptions: {
+        input: await glob('./src/**/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+    },
+    server: {
+      fs: {
+        allow: ['..'],
+      },
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
+});
