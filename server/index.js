657c7c09802305d7d74a9a8daff06632c9ea6604
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-09 22:39:28 +0100

    dsc

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.static('src'));
 // Віддаємо статичні файли з `src/`
-app.use(express.static(path.join(__dirname, "../src")));
+app.use(express.static(path.join(__dirname, "../dist")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../src/index.html"));
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 // Додаємо окремі маршрути для конкретних директорій
--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     _global: {},
   },
   root: 'src', 
-  publicDir: 'public',
+  publicDir: '../public',
   base: "./",
   build: {
     sourcemap: true,
