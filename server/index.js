51eeded4d3e9ad28c1c895bc5b7d167e799dad3d
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-10 13:18:16 +0100

    update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static('src'));
 app.use(express.static(path.join(__dirname, "../src")));
 
 
