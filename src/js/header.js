15d3cd65a79ea0555eec9b6d9a52120e26e70aea
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-10 19:22:23 +0100

    df

--- src/js/header.js
+++ src/js/header.js
@@ -17,6 +17,22 @@ window.addEventListener('scroll', () => {
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1); 
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth', 
+        block: 'start' 
+      });
+    }
+  });
+});
+
 // =============== active page ====================
 const currentPage = window.location.pathname.split("/").pop(); 
 const menuLinks = document.querySelectorAll('.nav-list a');
