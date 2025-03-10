b362913f834b33bf819b1d2d253cc85221c5217d
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-10 08:34:08 +0100

    build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,37 +1,70 @@
-/**
- * @type {import('vite').UserConfig}
- */
-import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+// /**
+//  * @type {import('vite').UserConfig}
+//  */
+// import { defineConfig } from 'vite';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+
+// export default defineConfig({
+//   define: {
+//     global: {},
+//     _global: {},
+//   },
+//   root: 'src',
+//   // publicDir: 'public',
+//   build: {
+//     sourcemap: true,
+//     outDir: '../dist',
+//     emptyOutDir: true,
+//     rollupOptions: {
+//      input: './src/index.html',
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             return 'vendor';
+//           }
+//         },
+//         entryFileNames: 'commonHelpers.js',
+//       },
+//     },
+//   },
+//   server: {
+//     fs: {
+//       allow: ['..'],
+//     },
+//   },
+//   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+// });
+import { defineConfig } from "vite";
+import htmlInject from "vite-plugin-html-inject";
+import fullReload from "vite-plugin-full-reload";
+import path from "path";
 
 export default defineConfig({
-  define: {
-    global: {},
-    _global: {},
-  },
-  root: 'src', 
-  // publicDir: 'public',
+  root: "src", // Коренева директорія фронтенду
+  publicDir: "src", // Доступ до статичних файлів
   build: {
-    sourcemap: true,
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: "../dist", // Деплойна директорія
+    emptyOutDir: true, // Очищення перед білдом
     rollupOptions: {
-     input: './src/index.html',
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        entryFileNames: 'commonHelpers.js',
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
       },
     },
   },
   server: {
-    fs: {
-      allow: ['..'], 
+    host: true, // Дозволяє доступ з локальної мережі
+    port: 5173, // Стандартний порт для Vite
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [
+    htmlInject(),
+    fullReload(["server/**/*.js", "server/**/*.mjs", "server/**/*.json"]),
+  ],
 });
