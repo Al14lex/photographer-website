575222cd6154927668a1b96355f0e68b6d077f6e
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-11 11:29:31 +0100

    last before prod

--- src/js/home/swiper.js
+++ src/js/home/swiper.js
@@ -3,31 +3,31 @@ import 'swiper/css';
 import { Pagination, Mousewheel } from 'swiper/modules';
 import 'swiper/css/pagination';
 
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const swiper = new Swiper('.swiper', {
+            modules: [Pagination, Mousewheel],
+            direction: 'horizontal',
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                dynamicBullets: true,
+            },
+            loop: true,
+            slidesPerView: 1,
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                },
+            },
+        });
 
-const swiper = new Swiper('.swiper', {
-  modules: [Pagination, Mousewheel],
-    direction: 'horizontal',
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        dynamicBullets: true,
-    },
-    loop: true,
-    mousewheel: {
-        invert: true, 
-    },
-
-     // Default parameters
-    slidesPerView: 1,
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 20
-        }
-    },
+        swiper.update();
+    }, 700);
 });
 
