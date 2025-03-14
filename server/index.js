70306f7dc565c0d63dbb3fdf9ed70006857bccfb
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-14 15:42:59 +0100

    fixed reviews error

--- server/index.js
+++ server/index.js
@@ -11,10 +11,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ["https://photographer-website.onrender.com", "https://www.aleksandraphoto.com"],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../src")));
 
