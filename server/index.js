92db8a94c534986bc99cc0fc4a585e442d205fad
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-03-22 10:53:03 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -20,13 +20,13 @@ app.use(cors({
 }));
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Expose-Headers", "ETag");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     res.header("Access-Control-Expose-Headers", "ETag");
+//     next();
+// });
 
 
 app.use(express.json());
